44d4aff69e24f281aa7cfa7843ad9d93426e4aad
Viktor Khotimchenko <viktorkhotimchenko@gmail.com>
2022-11-15 20:59:48 -0500

    wip

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,13 +1,41 @@
-use clap::Parser;
+// use clap::Parser;
 
-#[derive(Parser)]
-struct CLI {
-    pattern: String,
-    path: std::path::PathBuf
+// #[derive(Parser)]
+// struct CLI {
+//     pattern: String,
+//     path: std::path::PathBuf
+// }
+
+// fn main() {
+//     let args = CLI::parse();
+//     let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
+//     for line in content.lines() {
+//         if line.contains(&args.pattern) {
+//             println!("{}", line);
+//         }
+//     }
+// }
+
+#[derive(Debug)]
+struct CustomError(String);
+
+fn main() -> Result<(), CustomError> {
+    let path = "mek.mek";
+    let content = std::fs::read_to_string(path)
+        .map_err(|err| CustomError(format!("Error reading `{}`: {}", path, err)))?;
+    // println!("{}", content);
+    println!("file content: {}", content);
+return Ok(());
 }
 
-fn main() {
-    let args = CLI::parse();
-    println!("{}", args.pattern);
-    println!("{}", args.path.display());
-}
+// #[derive(Debug)]
+// struct CustomError(String);
+
+// fn main() -> Result<(), CustomError> {
+//     let path = "test.txt";
+//     let content = std::fs::read_to_string(path)
+//         .map_err(|err| CustomError(format!("Error reading `{}`: {}", path, err)))?;
+//     println!("file content: {}", content);
+//     Ok(())
+// }
