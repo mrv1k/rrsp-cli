6fb775f841ecd37119e7d93d746b9ee8c756cb88
Viktor Khotimchenko <viktorkhotimchenko@gmail.com>
2022-11-17 21:21:10 -0500

    wip

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,42 +1,32 @@
-// use clap::Parser;
+use clap::Parser;
 
-// #[derive(Parser)]
-// struct CLI {
-//     pattern: String,
-//     path: std::path::PathBuf
-// }
-
-// fn main() {
-//     let args = CLI::parse();
-//     let content = std::fs::read_to_string(&args.path).expect("could not read file");
-
-//     for line in content.lines() {
-//         if line.contains(&args.pattern) {
-//             println!("{}", line);
-//         }
-//     }
-// }
-
-use indicatif;
-
-
-fn main() {
-    let pb = indicatif::ProgressBar::new(100);
-    for i in 0..100 {
-        // doMekMek
-        pb.println(format!("[+] finished #{}", i));
-        pb.inc(1);
-    }
-    pb.finish_with_message("done");
+#[derive(Parser)]
+struct CLI {
+    pattern: String,
+    path: std::path::PathBuf
 }
 
-// #[derive(Debug)]
-// struct CustomError(String);
+fn main() {
+    let args = CLI::parse();
+    let content = std::fs::read_to_string(&args.path);
+    // find_matches(content, &args.pattern, stdout);
+}
 
-// fn main() -> Result<(), CustomError> {
-//     let path = "test.txt";
-//     let content = std::fs::read_to_string(path)
-//         .map_err(|err| CustomError(format!("Error reading `{}`: {}", path, err)))?;
-//     println!("file content: {}", content);
-//     Ok(())
-// }
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) { 
+    for line in content.lines() {
+        if line.contains(pattern) {
+            writeln!(writer, "{}", line);
+
+        }
+    }
+}
+
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+    find_matches("mek\n", "mek", &mut result);
+    assert_eq!(result, b"mek\n");
+
+    // find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+    // assert_eq!(result, b"lorem ipsum\n");
+}
