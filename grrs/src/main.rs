901b3bcd1fe7fee7feeea6cca46d8fd9a70caaf4
Viktor Khotimchenko <viktorkhotimchenko@gmail.com>
2022-11-18 22:11:31 -0500

    wip

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,22 +1,26 @@
 use clap::Parser;
+use anyhow::{Context, Result};
 
 #[derive(Parser)]
 struct CLI {
     pattern: String,
-    path: std::path::PathBuf
+    path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = CLI::parse();
-    let content = std::fs::read_to_string(&args.path);
-    // find_matches(content, &args.pattern, stdout);
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
+
+    find_matches(&content, &args.pattern, &mut std::io::stdout());
+
+    Ok(())
 }
 
-fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) { 
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
     for line in content.lines() {
         if line.contains(pattern) {
             writeln!(writer, "{}", line);
-
         }
     }
 }
