c0d6fa416542f079c10c38fad9e5623df55d917e
Viktor WSL <viktorkhotimchenko@gmail.com>
2022-11-20 21:37:34 -0500

    wip

--- grrs/tests/cli.rs
+++ grrs/tests/cli.rs
@@ -13,3 +13,19 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
+use assert_fs::prelude::*;
+
+#[test]
+fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    file.write_str("hello mekness\n my old\nmek")?;
+
+    let mut cmd = Command::cargo_bin("grrs")?;
+    cmd.arg("mek").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("hello mekness\nmek"));
+
+    Ok(())
+}
